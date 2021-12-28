d041243e1823ad4c0f414fb707b995e5195803f4
He Bo <nku_hb@outlook.com>
2021-12-28 17:28:20 +0800

    init commit

---
+++ windows_header.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <vector>
+using std::vector;
+
+#include <map>
+using std::map;
+#include <unordered_map>
+using std::unordered_map;
+
+#include <algorithm>
+using std::sort;
+
+#include <set>
+using std::set;
+#include <unordered_set>
+using std::unordered_set;
+
+#include <string>
+using std::string;
+
+
+using std::lower_bound;
+using std::upper_bound;
+using std::distance;
+using std::ios;
